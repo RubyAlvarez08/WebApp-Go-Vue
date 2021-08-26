e1cb5703f1e2f468ee8beaa8cfad7863fe9adb4b
Ruby <alvarezruby098@gmail.com>
2021-08-26 11:38:40 -0500

    creacion de archivos staticos

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http" //para levantar un servidor
+	"https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers"
+	
 
+)
 func main () {
 	log.Println("hello")
 }
