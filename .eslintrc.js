6909397bdac00fcf0e12e4db1fd82a2b94b62aa8
Ruby <alvarezruby098@gmail.com>
2021-08-27 16:35:02 -0500

    instalacion e inicializacion con vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
   },
-  'extends': [
-    'plugin:vue/essential',
-    'google',
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
   ],
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'vue',
+  // required to lint *.vue files
+  plugins: [
+    'vue'
   ],
-  'rules': {
-  },
-};
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
